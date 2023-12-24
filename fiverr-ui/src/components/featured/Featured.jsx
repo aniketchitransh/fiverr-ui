e99d655472c2fb473508a10e9f4d4497483c5518
Aniket Chitransh <ani.chitransh@gmail.com>
2023-12-24 12:19:49 +0530

    Imported Images

--- fiverr-ui/src/components/featured/Featured.jsx
+++ fiverr-ui/src/components/featured/Featured.jsx
@@ -5,7 +5,24 @@ const Featured = () => {
     return (
         <div className='featured'>
             <div className="container">
-                
+                <div className="left">
+                    <h1>Find the perfect freelance services for your business</h1>
+                    <div className="search">
+                        <div className="searchInput">
+                            <img src="" alt="" />
+                            <input type="text" placeholder='Try "Building Mobile App"' />
+                        </div>
+                        <button>Search</button>
+                    </div>
+                    <div className="popular">
+                        <span>Popular:</span>
+                        <button>Web Design</button>
+                        <button>Wordpress</button>
+                        <button>Logo Design</button>
+                        <button>AI Services</button>
+                    </div>
+                </div>
+                <div className="right"></div>
             </div>
         </div>
     )
