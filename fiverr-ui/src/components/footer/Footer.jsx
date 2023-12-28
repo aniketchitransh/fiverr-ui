221a0dd8765f95e1ce1a07a08a26e9eb746843bf
Aniket Chitransh <ani.chitransh@gmail.com>
2023-12-28 16:50:18 +0530

    Setup complete for footer. Now adding scss to thr footer.scss file

--- fiverr-ui/src/components/footer/Footer.jsx
+++ fiverr-ui/src/components/footer/Footer.jsx
@@ -67,9 +67,30 @@ const Footer = () => {
                         <span>Contact Sales</span>
                     </div>
                 </div>
+                <hr />
                 <div className="bottom">
-                    <div className="left"></div>
-                    <div className="right"></div>
+                    <div className="left">
+                        <h2>fiverr</h2>
+                        <span>© Fiverr International Ltd. 2023</span>
+                    </div>
+                    <div className="right">
+                        <div className="social">
+                            <img src="img/twitter.png" alt="" />
+                            <img src="img/facebook.png" alt="" />
+                            <img src="img/linkedin.png" alt="" />
+                            <img src="img/pinterest.png" alt="" />
+                            <img src="img/instagram.png" alt="" />
+                        </div>
+                        <div className="links">
+                            <img src="./img/language.png" alt="" />
+                            <span>English</span>
+                        </div>
+                        <div className="links">
+                            <img src="./img/coin.png" alt="" />
+                            <span>USD</span>
+                        </div>
+                        <img src="./img/accessibility.png" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
