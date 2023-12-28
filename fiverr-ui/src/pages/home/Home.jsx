1e7e21c650d6a8bf507789cdddf206a7792922b6
Aniket Chitransh <ani.chitransh@gmail.com>
2023-12-28 12:32:31 +0530

    Feature Component design complete

--- fiverr-ui/src/pages/home/Home.jsx
+++ fiverr-ui/src/pages/home/Home.jsx
@@ -11,7 +11,31 @@ const Home = () => {
       <div className="features">
         <div className="container">
           <div className="item">
-            <h1>A whole world of freelance talent at your fingertips</h1>
+            <h1>A whole world of freelance talent at <br />your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at your price point. No hourly rates.
+              Just project based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at your price point. No hourly rates.
+              Just project based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at your price point. No hourly rates.
+              Just project based pricing.
+            </p>
             <div className="title">
               <img src="./img/check.png" alt="" />
               The best for every budget
@@ -23,6 +47,37 @@ const Home = () => {
           </div>
           <div className="item">
             <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <div className="logo">
+              <span className='text'>fiverr</span>
+              <span className='dot'>.</span>
+              <span className='st'>Bussiness Solutions</span>
+            </div>
+            <h1>Advanced solutions and professional talent for businesses</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven bussiness experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven bussiness experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven bussiness experience
+            </div>
+            <button>Learn More</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/51c35c7cecf75e6a5a0110d27909a2f5-1690202609364/EN.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
