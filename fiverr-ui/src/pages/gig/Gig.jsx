cccb885af800b79e01ba013e9837078fa3a98bb7
Aniket Chitransh <ani.chitransh@gmail.com>
2023-12-29 14:38:40 +0530

    Setup of the page till reviews section

--- fiverr-ui/src/pages/gig/Gig.jsx
+++ fiverr-ui/src/pages/gig/Gig.jsx
@@ -3,7 +3,248 @@ import './Gig.scss'
 
 const Gig = () => {
   return (
-    <div className='gig'>Gig</div>
+    <div className='gig'>
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumbs">Fiver : Graphic & Design :</span>
+          <h1>I will create AI generated art for you</h1>
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <span>John Doe</span>
+            <div className="stars">
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+          <h2>About This Gig</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Facilis suscipit quasi eum corrupti asperiores ea!
+            Similique odit temporibus possimus voluptas veritatis esse,
+            iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Facilis suscipit quasi eum corrupti asperiores ea!
+            Similique odit temporibus possimus voluptas veritatis esse,
+            iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Facilis suscipit quasi eum corrupti asperiores ea!
+          </p>
+          <div className="seller">
+            <h2>About The Seller</h2>
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="info">
+                <span>John Doe</span>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <button>Contact Me</button>
+              </div>
+            </div>
+            <div className="box">
+              <div className="items">
+                <div className="item">
+                  <div className="title">From</div>
+                  <div className="desc">USA</div>
+                </div>
+                <div className="item">
+                  <div className="title">From</div>
+                  <div className="desc">USA</div>
+                </div>
+                <div className="item">
+                  <div className="title">From</div>
+                  <div className="desc">USA</div>
+                </div>
+                <div className="item">
+                  <div className="title">From</div>
+                  <div className="desc">USA</div>
+                </div>
+                <div className="item">
+                  <div className="title">From</div>
+                  <div className="desc">USA</div>
+                </div>
+                <hr />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis suscipit quasi eum corrupti asperiores ea!
+                  Similique odit temporibus possimus voluptas veritatis esse,
+                  iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis suscipit quasi eum corrupti asperiores ea!
+                  Similique odit temporibus possimus voluptas veritatis esse,
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+                iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+                iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+                iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+                iure beatae voluptate unde. Consequatur consectetur accusantium repellat!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis suscipit quasi eum corrupti asperiores ea!
+                Similique odit temporibus possimus voluptas veritatis esse,
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
