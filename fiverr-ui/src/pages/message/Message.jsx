ed1dcb9161164693722c5f83c51fe8004a16fa6b
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-04 19:06:07 +0530

    Starting with single message page

--- fiverr-ui/src/pages/message/Message.jsx
+++ fiverr-ui/src/pages/message/Message.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import'./Message.scss'
+import { Link } from 'react-router-dom'
 
 const Message = () => {
   return (
-    <div className='message'>Message</div>
+    <div className='message'>
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">Messages</Link> : John Doe
+        </span>
+        <div className="messages"></div>
+        <div className="write"></div>
+      </div>
+    </div>
   )
 }
 
