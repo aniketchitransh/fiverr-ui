e3967f5cf3b71a3debbce0719a84f8c12917c21e
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-04 12:36:54 +0530

    Setingup messages page

--- fiverr-ui/src/pages/messages/Messages.jsx
+++ fiverr-ui/src/pages/messages/Messages.jsx
@@ -2,8 +2,99 @@ import React from 'react'
 import './Messages.scss'
 
 const Messages = () => {
+  const currentUser = {
+    id: 1,
+    username: "John Deo",
+    isSeller: true
+  }
+
   return (
-    <div className='messages'>Messages</div>
+    <div className='messages'>
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{ currentUser?.isSeller ? "Buyer" : "Seller" }</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className='img'
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className='img'
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className='img'
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className='img'
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className='img'
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
