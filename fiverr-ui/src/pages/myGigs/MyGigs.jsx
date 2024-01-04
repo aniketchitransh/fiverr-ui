2f9468fcf4b8e3dcfe3d6db31cd2a70049f4a9e4
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-04 11:41:46 +0530

    Using HTML table to insert table in the myGigs page

--- fiverr-ui/src/pages/myGigs/MyGigs.jsx
+++ fiverr-ui/src/pages/myGigs/MyGigs.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './MyGigs.scss'
+import { Link } from 'react-router-dom'
 
 const MyGigs = () => {
   return (
     <div className='myGigs'>
       <div className="container">
-        
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to="/add">Add New Gig</Link>
+        </div>
+        <table>
+          
+        </table>
       </div>
     </div>
   )
