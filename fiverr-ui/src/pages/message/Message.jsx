480a61aa184007a3e70e783ae6ea4e653daa12b0
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-04 19:31:04 +0530

    Completed chat page design

--- fiverr-ui/src/pages/message/Message.jsx
+++ fiverr-ui/src/pages/message/Message.jsx
@@ -9,8 +9,85 @@ const Message = () => {
         <span className="breadcrumbs">
           <Link to="/messages">Messages</Link> : John Doe
         </span>
-        <div className="messages"></div>
-        <div className="write"></div>
+        <div className="messages">
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Natus exercitationem
+              adipisci perspiciatis delectus. Neque
+              repellendus iure perferendis id libero fugiat.
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="write">
+          <textarea name="" placeholder='write a message' id="" cols="30" rows="10"></textarea>
+          <button>Send</button>
+        </div>
       </div>
     </div>
   )
